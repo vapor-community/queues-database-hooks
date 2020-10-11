0dbcf1d7a468eb27eea6a0a1f2ab14614f48df1b
Jimmy McDermott <mcdappdev@gmail.com>
2020-10-10 23:34:16 -0400

    Bytes

--- Sources/QueuesDatabaseHooks/QueuesDatabaseNotificationHook.swift
+++ Sources/QueuesDatabaseHooks/QueuesDatabaseNotificationHook.swift
@@ -35,7 +35,7 @@ public struct QueuesDatabaseNotificationHook: NotificationHook {
     public func dispatched(job: NotificationJobData, eventLoop: EventLoop) -> EventLoopFuture<Void> {
         QueueDatabaseEntry(jobId: job.id,
                            jobName: job.jobName,
-                           payload: Data(),
+                           payload: Data(job.payload),
                            maxRetryCount: job.maxRetryCount,
                            delayUntil: job.delayUntil,
                            queuedAt: job.queuedAt,
