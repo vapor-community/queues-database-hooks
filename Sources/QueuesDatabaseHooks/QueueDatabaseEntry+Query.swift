fcae29d31fb51eb0ee84cf18b8f4e479c4a7069f
Gwynne Raskind <gwynne@darkrainfall.org>
2021-10-22 07:21:13 -0500

    Retool the database hook so that all queries are valid on all databases (not just SQL databases) and use queries for status transitions that yield a sensible state even if some notifications are missed or arrive out of order. Also updates documentation comments in many places and adds a migration to add a uniqueness constraint to the job ID in the database.

--- Sources/QueuesDatabaseHooks/QueueDatabaseEntry+Query.swift
+++ Sources/QueuesDatabaseHooks/QueueDatabaseEntry+Query.swift
@@ -5,99 +5,124 @@ import Vapor
 
 public extension QueueDatabaseEntry {
 
-    /// Returns the current queued count of jobs and current running count
-    /// - Parameter db: The Database to run the query on - must be a `SQLDatabase`
-    /// - Returns: The data returned from the query
+    /// Returns the current counts of queued and running jobs.
+    /// - Parameters:
+    ///   - db: The Database to run the query on.
+    /// - Returns: A future whose result is a structure containing the counts of queued and running jobs at the time of
+    ///   the query.
     static func getStatusOfCurrentJobs(db: Database) -> EventLoopFuture<CurrentJobsStatusResponse> {
-        guard let sqlDb = db as? SQLDatabase else { return db.eventLoop.future(error: Abort(.badRequest, reason: "Only SQL Databases Supported")) }
-
-        let query: SQLQueryString = """
-        SELECT
-            COALESCE(
-                SUM(
-                    CASE status
-                    WHEN 0 THEN
-                        1
-                    ELSE
-                        0
-                    END
-                )
-            , 0) as "queuedCount",
-            COALESCE(
-                SUM(
-                    CASE status
-                    WHEN 1 THEN
-                        1
-                    ELSE
-                        0
-                    END
-                )
-            , 0) as "runningCount"
-        FROM
-            _queue_job_completions
-        """
-
-        return sqlDb.raw(query).first(decoding: CurrentJobsStatusResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
-    }
-
-    /// Retrieves data about jobs that ran successfully over the specified time period
-    /// - Parameters:
-    ///   - db: The Database to run the query on - must be a `SQLDatabase`
-    ///   - hours: The number of previous hours to check (i.e. if `1` is specified, it will retrieve data for the past 1 hour)
-    /// - Returns: The data returned from the query
-    static func getCompletedJobsForTimePeriod(db: Database, hours: Int) -> EventLoopFuture<CompletedJobStatusResponse> {
-        guard let sqlDb = db as? SQLDatabase else { return db.eventLoop.future(error: Abort(.badRequest, reason: "Only SQL Databases Supported")) }
-
-        let query: SQLQueryString = """
-        SELECT
-            COUNT(*) as "completedJobs",
-            COALESCE(SUM(
-                CASE status
-                WHEN 2 THEN
-                    1
-                ELSE
-                    0
-                END) / count(*), 1) as "percentSuccess"
-        FROM
-            _queue_job_completions
-        WHERE
-            completedAt IS NOT NULL
-            AND completedAt >= DATE_SUB(now(), interval \(raw: "\(hours)") hour)
-        """
-
-        return sqlDb.raw(query).first(decoding: CompletedJobStatusResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
-    }
-
-    /// Retrieves data about the how quickly jobs ran and how long they waited to be run
-    /// - Parameters:
-    ///   - db: The Database to run the query on - must be a `SQLDatabase`
-    ///   - hours: The number of previous hours to check (i.e. if `1` is specified, it will retrieve data for the past 1 hour)
-    ///   - jobName: The name of the job to filter on, if any
-    /// - Returns: The data returned from the query
-    static func getTimingDataForJobs(db: Database, hours: Int, jobName: String? = nil) -> EventLoopFuture<JobsTimingResponse> {
-        guard let sqlDb = db as? SQLDatabase else { return db.eventLoop.future(error: Abort(.badRequest, reason: "Only SQL Databases Supported")) }
-
-        let jobFilterString: SQLQueryString
-        if let jobName = jobName {
-            jobFilterString = "AND jobName = \(raw: jobName)"
+        if let sql = db as? SQLDatabase {
+            /// This syntax is compatible with all supported SQL databases at the time of this writing.
+            return sql.raw("""
+                SELECT
+                    COALESCE(SUM(IF(\(ident: "status")=\(literal: 0),\(literal: 1),\(literal: 0))),\(literal: 0)) AS \(ident: "queuedCount"),
+                    COALESCE(SUM(IF(\(ident: "status")=\(literal: 1),\(literal: 1),\(literal: 0))),\(literal: 0)) AS \(ident: "runningCount")
+                FROM
+                    \(ident: QueueDatabaseEntry.schema)
+                """)
+            .first(decoding: CurrentJobsStatusResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
         } else {
-            jobFilterString = ""
+            return QueueDatabaseEntry.query(on: db).filter(\.$status == .queued).count(\.$id).flatMap { queuedCount in
+                QueueDatabaseEntry.query(on: db).filter(\.$status == .running).count(\.$id).map { runningCount in
+                    .init(queuedCount: queuedCount, runningCount: runningCount)
+                }
+            }
         }
+    }
 
-        let query: SQLQueryString = """
-        SELECT
-            avg(TIMESTAMPDIFF(second, dequeuedAt, completedAt)) as "avgRunTime",
-            avg(TIMESTAMPDIFF(second, queuedAt, dequeuedAt)) as "avgWaitTime"
-        FROM
-            _queue_job_completions
-        WHERE
-            completedAt IS NOT NULL
-            AND dequeuedAt is not null
-            AND completedAt >= DATE_SUB(now(), interval \(raw: "\(hours)") hour)
-            \(jobFilterString)
-        """
+    /// Retrieves data about jobs that ran successfully over the specified time period.
+    ///
+    /// - Parameters:
+    ///   - db: The Database to run the query on.
+    ///   - hours: The maximum age in hours for a job to be considered. For example, a value of `1` will retrieve data
+    ///     only for jobs whose completion date is within the past hour. Negative numbers and `0` are illegal inputs.
+    /// - Returns: A future whose result is a structure containing the count of completed jobs and the percentage of
+    ///   those jobs which completed successfully within the past given number of hours, as of the time of the query.
+    static func getCompletedJobsForTimePeriod(db: Database, hours: Int) -> EventLoopFuture<CompletedJobStatusResponse> {
+        precondition(hours > 0, "Can not request job data for jobs in the future.")
+        
+        let deadline = Calendar.current.date(byAdding: .hour, value: -hours, to: Date(), wrappingComponents: true)
 
-        return sqlDb.raw(query).first(decoding: JobsTimingResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
+        if let sql = db as? SQLDatabase {
+            return sql.raw("""
+                SELECT
+                    COUNT(\(ident: "id")) AS \(ident: "completedJobs"),
+                    COALESCE(SUM(IF(\(ident: "status")=\(literal: 2),\(literal: 1),\(literal: 0))) / COUNT(\(ident: "id")),\(literal: 1))
+                        AS \(ident: "percentSuccess")
+                FROM
+                    \(ident: QueueDatabaseEntry.schema)
+                WHERE
+                    \(ident: "completedAt")>=\(bind: deadline)
+                """)
+            .first(decoding: CompletedJobStatusResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
+        } else {
+            return QueueDatabaseEntry.query(on: db).filter(\.$completedAt >= deadline).count(\.$id).flatMap { completedJobs in
+                QueueDatabaseEntry.query(on: db).filter(\.$completedAt >= deadline).filter(\.$status == .success).count(\.$id).map { successfulJobs in
+                    .init(completedJobs: completedJobs, percentSuccess: Double(successfulJobs) / Double(completedJobs))
+                }
+            }
+        }
+    }
+
+    /// Retrieves data about the how quickly jobs ran and how long they waited to be run.
+    ///
+    /// - Warning: At the time of this writing, due to limitations of Fluent, this query may run _very_ slowly if used
+    ///   with a NoSQL database driver like Mongo, depending on the number of jobs recorded in the database and the
+    ///   given filtering parameters.
+    ///
+    /// - Parameters:
+    ///   - db: The Database to run the query on.
+    ///   - hours: The maximum age in hours for a job to be considered. For example, a value of `1` will retrieve data
+    ///     only for jobs whose completion date is within the past hour. Negative numbers and `0` are illegal inputs.
+    ///   - jobName: The name of the job to filter on, if any
+    /// - Returns: A future whose result is a structure containing the average number of seconds it took the specified
+    ///   jobs to run and that the specified jobs spent queued before running within the past given number of hours, as
+    ///   of the time of the query.
+    static func getTimingDataForJobs(db: Database, hours: Int, jobName: String? = nil) -> EventLoopFuture<JobsTimingResponse> {
+        precondition(hours > 0, "Can not request job data for jobs in the future.")
+        
+        let deadline = Calendar.current.date(byAdding: .hour, value: -hours, to: Date(), wrappingComponents: true)
+
+        if let sql = db as? SQLDatabase {
+            let jobNameClause: SQLQueryString = jobName.map { "\(ident: "jobName")=\(bind: $0)" } ?? "\(literal: 1)=\(literal: 1)"
+            let dateDiffExpression: (String, String) -> SQLQueryString
+            switch sql.dialect.name {
+                case "mysql":      dateDiffExpression = { "TIMESTAMPDIFF(SECOND, \(ident: $1), \(ident: $0))" }
+                case "postgresql": dateDiffExpression = { "extract(epoch from \(ident: $0) - \(ident: $1))" }
+                case "sqlite":     dateDiffExpression = { "strftime(\(literal: "%s"), \(ident: $0)) - strftime(\(literal: "%s"), \(ident: $1))" }
+                case let name:
+                    /// Because people are stubborn, only crash in Debug.
+                    assertionFailure("You're using an unsupported SQL dialect (\(name)), try again.")
+                    return sql.eventLoop.makeFailedFuture(Abort(.internalServerError, reason: "Unsupported SQL database dialect '\(name)'"))
+            }
+            
+            return sql.raw("""
+                SELECT
+                    COALESCE(AVG(\(dateDiffExpression("completedAt", "dequeuedAt"))), \(literal: 0)) AS \(ident: "avgRunTime"),
+                    COALESCE(AVG(\(dateDiffExpression("dequeuedAt", "queuedAt"))), \(literal: 0)) AS \(ident: "avgWaitTime")
+                FROM
+                    \(ident: QueueDatabaseEntry.schema)
+                WHERE
+                    \(ident: "dequeuedAt") IS NOT \(SQLLiteral.null) AND
+                    \(ident: "completedAt")>=\(bind: deadline) AND
+                    \(jobNameClause)
+                """)
+            .first(decoding: JobsTimingResponse.self).unwrap(or: Abort(.badRequest, reason: "Could not get data for status"))
+        } else {
+            return QueueDatabaseEntry.query(on: db)
+                .field(\.$queuedAt).field(\.$dequeuedAt).field(\.$completedAt)
+                .filter(\.$completedAt >= deadline)
+                .filter(\.$dequeuedAt != nil)
+                .filter(\.$jobName, .equality(inverse: jobName == nil), jobName ?? "")
+                .all()
+            .nonemptyMap(or: .init(avgRunTime: nil, avgWaitTime: nil)) {
+                let sums = $0.reduce((0.0, 0.0)) { s, q in (
+                    s.0 + (q.completedAt!.timeIntervalSinceReferenceDate - q.dequeuedAt!.timeIntervalSinceReferenceDate),
+                    s.1 + (q.dequeuedAt!.timeIntervalSinceReferenceDate - q.queuedAt.timeIntervalSinceReferenceDate)
+                ) }
+                return .init(avgRunTime: sums.0 / Double($0.count), avgWaitTime: sums.1 / Double($0.count))
+            }
+        }
     }
 }
 
--- Sources/QueuesDatabaseHooks/QueueDatabaseEntry.swift
+++ Sources/QueuesDatabaseHooks/QueueDatabaseEntry.swift
@@ -1,77 +1,90 @@
 import Foundation
 import FluentKit
+import SQLKit
 
-/// Stores information about a `Queue` job
-/// A record gets added when the job is dispatched
-/// and then updated with its status when it succeeds or fails
+/// Stores information about a `Queue` job.
+///
+/// A record is added when the job is dispatched and updated with the final status
+/// when it succeeds or fails.
 public final class QueueDatabaseEntry: Model {
     public static let schema = "_queue_job_completions"
 
+    /// The database primary key.
     @ID(key: .id)
     public var id: UUID?
 
-    /// The `jobId` that came from the queues package
+    /// The `jobId` from the `queues` package.
     @Field(key: "jobId")
     public var jobId: String
 
-    /// The name of the job
+    /// The name of the job.
     @Field(key: "jobName")
     public var jobName: String
 
-    /// The queue the job was run on
+    /// The name of the queue on which the job was run.
     @Field(key: "queueName")
     public var queueName: String
 
-    /// The data associated with the job
+    /// The data associated with the job.
     @Field(key: "payload")
     public var payload: Data
 
-    /// The retry count for the job
+    /// The job's retry count.
     @Field(key: "maxRetryCount")
     public var maxRetryCount: Int
 
-    /// The `delayUntil` date from the queues package
+    /// The `delayUntil` date from the `queues` package.
     @OptionalField(key: "delayUntil")
     public var delayUntil: Date?
 
-    /// The date the job was queued at
+    /// The `queuedAt` timestamp from the `queues` package.
     @Field(key: "queuedAt")
     public var queuedAt: Date
 
-    /// The date the job was dequeued at
-    @OptionalField(key: "dequeuedAt")
+    /// The timestamp at which the job was dequeued, or `nil` if it is still queued.
+    @Timestamp(key: "dequeuedAt", on: .none)
     public var dequeuedAt: Date?
 
-    /// The date the job was completed
-    @OptionalField(key: "completedAt")
+    /// The timestamp at which the job was completed, or `nil` if it is not complete.
+    ///
+    /// A job is considered complete regardless of success or failure.
+    ///
+    /// - Precondition: If this property is not `nil`, `dequeuedAt` must not be `nil`.
+    @Timestamp(key: "completedAt", on: .none)
     public var completedAt: Date?
 
-    /// The error string for the job
+    /// The error string for the job, or `nil` if it succeeded or is not yet complete.
+    ///
+    /// - Precondition: If this property is not `nil`, `completedAt` must not be `nil`.
     @OptionalField(key: "errorString")
     public var errorString: String?
 
-    /// The status of the job
+    /// The status of the job.
     @Field(key: "status")
     public var status: Status
 
+    /// The timestamp at which the database record of the job was created.
+    ///
+    /// This should usually be within a few ms of `queuedAt`, unless under heavy load.
     @Timestamp(key: "createdAt", on: .create)
     public var createdAt: Date?
 
+    /// The timestamp at which the database record was last updated.
     @Timestamp(key: "updatedAt", on: .update)
     public var updatedAt: Date?
 
-    /// The status of the queue job
-    public enum Status: Int, Codable {
-        /// The job has been queued but not yet picked up for processing
+    /// A queue job's status.
+    public enum Status: Int, CaseIterable, Codable {
+        /// The job is queued but not yet picked up for processing.
         case queued
 
-        /// The job has been moved ot the processing queue and is currently running
+        /// The job has been moved to the processing queue and is currently running.
         case running
 
-        /// The job has finished and it was successful
+        /// The job was completed successfully.
         case success
 
-        /// The job has finished and it returned an error
+        /// The job failed.
         case error
     }
 
@@ -104,6 +117,62 @@ public final class QueueDatabaseEntry: Model {
     }
 }
 
+extension QueueDatabaseEntry {
+    /// It is a hard error to queue the same `jobId` more than once.
+    internal static func recordDispatch(
+        jobId: String, jobName: String, queueName: String, payload: Data,
+        maxRetryCount: Int, delayUntil: Date?, dispatchTimestamp: Date,
+        on database: Database
+    ) -> EventLoopFuture<Void> {
+        return QueueDatabaseEntry(jobId: jobId, jobName: jobName, queueName: queueName, payload: payload, maxRetryCount: maxRetryCount, delayUntil: delayUntil, queuedAt: dispatchTimestamp, dequeuedAt: nil, completedAt: nil, errorString: nil, status: .queued).create(on: database)
+    }
+    
+    /// Dequeuing an already-running or completed job only sets the dequeuing timestamp iff it wasn't already set.
+    internal static func recordDequeue(
+        jobId: String, dequeueTimestamp: Date, on database: Database
+    ) -> EventLoopFuture<Void> {
+        if let sql = database as? SQLDatabase {
+            return sql.raw("""
+                UPDATE \(ident: QueueDatabaseEntry.schema)
+                SET
+                    \(ident: "dequeuedAt")=CASE WHEN \(ident: "dequeuedAt") IS NULL THEN \(bind: dequeueTimestamp) ELSE \(ident: "dequeuedAt") END,
+                    \(ident: "status")=CASE WHEN \(ident: "status")=\(bind: Status.queued) THEN \(bind: Status.running) ELSE \(ident: "status") END
+                WHERE
+                    \(ident: "jobId")=\(bind: jobId)
+                """)
+                .run()
+        } else {
+            return QueueDatabaseEntry.query(on: database).set(\.$dequeuedAt, to: dequeueTimestamp).filter(\.$dequeuedAt == nil).filter(\.$jobId == jobId).update()
+                .flatMap { QueueDatabaseEntry.query(on: database).set(\.$status, to: .running).filter(\.$status == .queued).filter(\.$jobId == jobId).update() }
+        }
+    }
+    
+    /// Completing an already-completed job has no effect, even if the final status differs (only the first completion
+    /// takes effect, in other words). Completing a still-queued job updates the dequeuing timestamp as well.
+    internal static func recordCompletion(
+        jobId: String, completionTimestamp: Date, errorString: String?, on database: Database
+    ) -> EventLoopFuture<Void> {
+        if let sql = database as? SQLDatabase {
+            return sql.raw("""
+                UPDATE \(ident: QueueDatabaseEntry.schema)
+                SET
+                    \(ident: "dequeuedAt")=CASE WHEN \(ident: "dequeuedAt") IS NULL THEN \(bind: completionTimestamp) ELSE \(ident: "dequeuedAt") END,
+                    \(ident: "completedAt")=CASE WHEN \(ident: "completedAt") IS NULL THEN \(bind: completionTimestamp) ELSE \(ident: "completedAt") END,
+                    \(ident: "errorString")=CASE WHEN \(ident: "errorString") IS NULL THEN \(bind: errorString) ELSE \(ident: "errorString") END,
+                    \(ident: "status")=CASE WHEN \(ident: "status") IN (\(bind: Status.queued), \(bind: Status.running)) THEN \(bind: errorString == nil ? Status.success : Status.error) ELSE \(ident: "status") END
+                WHERE
+                    \(ident: "jobId")=\(bind: jobId)
+                """)
+                .run()
+        } else {
+            return QueueDatabaseEntry.query(on: database).set(\.$dequeuedAt, to: completionTimestamp).filter(\.$dequeuedAt == nil).filter(\.$jobId == jobId).update()
+                .flatMap { QueueDatabaseEntry.query(on: database).set(\.$completedAt, to: completionTimestamp).filter(\.$completedAt == nil).filter(\.$jobId == jobId).update() }
+                .flatMap { QueueDatabaseEntry.query(on: database).set(\.$errorString, to: errorString).filter(\.$errorString == nil).filter(\.$jobId == jobId).update() }
+                .flatMap { QueueDatabaseEntry.query(on: database).set(\.$status, to: errorString == nil ? .success : .error).filter(\.$status ~~ [.queued, .running]).filter(\.$jobId == jobId).update() }
+        }
+    }
+}
+
 public struct QueueDatabaseEntryMigration: Migration {
     public init() { }
 
@@ -123,10 +192,30 @@ public struct QueueDatabaseEntryMigration: Migration {
             .field("status", .int8, .required)
             .field("createdAt", .datetime)
             .field("updatedAt", .datetime)
+            .unique(on: "jobId")
             .create()
     }
 
     public func revert(on database: Database) -> EventLoopFuture<Void> {
         database.schema(QueueDatabaseEntry.schema).delete()
+    }
+}
+
+/// A migration intended for users updating from 0.2.0 to any later prerelease version.
+///
+/// - Important: If a 1.0.0 release happens, delete this first!
+public struct QueueDatabaseEntryUpgradeFrom_0_2_0: Migration {
+    public init() { }
+    
+    public func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema(QueueDatabaseEntry.schema)
+            .unique(on: "jobId")
+            .update()
+    }
+    
+    public func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema(QueueDatabaseEntry.schema)
+            .deleteUnique(on: "jobId")
+            .update()
     }
 }
