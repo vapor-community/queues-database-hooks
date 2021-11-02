6582acd32ca5979ea8a8ed798b93851a5968ea53
Jesse Tipton <jesse@jessetipton.com>
2021-11-01 23:17:48 -0700

    Add Postgres tests

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         
         // Test-only dependencies
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
     ],
@@ -34,6 +35,7 @@ let package = Package(
             dependencies: [
                 .target(name: "QueuesDatabaseHooks"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "XCTQueues", package: "queues"),
                 .product(name: "XCTVapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
